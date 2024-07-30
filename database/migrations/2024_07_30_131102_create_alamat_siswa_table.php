73834bad6747ab5e2fc1da641806682d81f4efb0
hasbi1028 <hasbi.awal@gmail.com>
2024-07-30 22:38:58 +0700

    Revert "lupa 1 tabel pada modul siswa"
    This reverts commit ac8ab89fc1496f26e03491c3d0b97fcadaba71b4.

--- database/migrations/2024_07_30_131102_create_alamat_siswa_table.php
+++ database/migrations/2024_07_30_131102_create_alamat_siswa_table.php
@@ -4,39 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAlamatSiswaTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('alamat_siswa', function (Blueprint $table) {
+        Schema::create('alamat_siswas', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('siswa_id');
-            $table->string('status_rumah_siswa', 50)->nullable();
-            $table->string('provinsi_siswa', 50)->nullable();
-            $table->string('kabupaten_siswa', 50)->nullable();
-            $table->string('kecamatan_siswa', 50)->nullable();
-            $table->string('kelurahan_siswa', 50)->nullable();
-            $table->string('rt_rw_siswa', 20)->nullable();
-            $table->text('alamat_lengkap_siswa')->nullable();
-            $table->string('kodepos_siswa', 10)->nullable();
-            $table->string('koordinat', 50)->nullable();
-
-            $table->foreign('siswa_id')->references('id')->on('siswa')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('alamat_siswa');
+        Schema::dropIfExists('alamat_siswas');
     }
-}
+};
