23579da1075f66e420a94bb82c553fd337a9490a
hasbi1028 <hasbi.awal@gmail.com>
2024-07-30 22:39:04 +0700

    Revert "modul data siswa selesai"
    This reverts commit 428c1ba201c08b40e4b7824422191f8afe6ffa2c.

--- database/migrations/2024_07_30_120654_create_riwayat_keaktifan_siswa_table.php
+++ database/migrations/2024_07_30_120654_create_riwayat_keaktifan_siswa_table.php
@@ -4,34 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRiwayatKeaktifanSiswaTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('riwayat_keaktifan_siswa', function (Blueprint $table) {
+        Schema::create('riwayat_keaktifan_siswas', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('siswa_id');
-            $table->string('tahun_ajaran', 20)->nullable();
-            $table->string('semester', 10)->nullable();
-            $table->string('status_keaktifan', 50)->nullable();
-            $table->text('keterangan')->nullable();
-
-            $table->foreign('siswa_id')->references('id')->on('siswa')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('riwayat_keaktifan_siswa');
+        Schema::dropIfExists('riwayat_keaktifan_siswas');
     }
-}
+};
